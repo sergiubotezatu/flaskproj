d1185211951009532cdadfdae01b02acc5681c29
sergiu.botezatu95@gmail.com <sergiu.botezatu95@gmail.com>
2022-02-15 06:41:25 +0200

    check current_app.config

--- view/Home.py
+++ view/Home.py
@@ -1,9 +1,10 @@
 from flask import Blueprint, render_template, current_app
 from services import seed, posts_factory
+import blog
 
 home = Blueprint("home", __name__)
-source_factory = posts_factory.Create(current_app.config["TESTING"])
-blogPosts = source_factory.create_source
+source_factory = posts_factory.Create()
+blogPosts = source_factory.create_source(current_app.config["testing"])
 
 @home.route("/")
 def front_page():
