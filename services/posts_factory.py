f70842f6024c892fc03967edb4f7756ece2a7ce1
sergiu.botezatu95@gmail.com <sergiu.botezatu95@gmail.com>
2022-02-22 10:13:08 +0200

    static factory

--- services/posts_factory.py
+++ services/posts_factory.py
@@ -1,11 +1,11 @@
 from services.posts_db_repo import PostsDb
 from services.seed import blogPosts
 
-class Create:
+class PostsFactory:
     def __init__(self, is_test):
         self.is_test = is_test
         
-    def create_source(self):
+    def create(self):
         if (self.is_test):
             return blogPosts
         return PostsDb()
