4d52c7e454fbe18fb021573e6cd9e1125585f643
sergiu.botezatu95@gmail.com <sergiu.botezatu95@gmail.com>
2022-02-24 00:27:16 +0200

    no def in def in bp classes

--- view/Home.py
+++ view/Home.py
@@ -4,23 +4,17 @@ from services.seed import placeholder
 class Home:
     def __init__(self, factory):
         self.blogPosts = factory
-        self.bp = self.create_bp()
+        self.bp = Blueprint("home", __name__)
+        self.to_db_setup = self.bp.before_request(self.goto_db_setup)
+        self.home = self.bp.route("/")(self.front_page)
+
+    def goto_db_setup(self):
+        if str(type(self.blogPosts)).find("PostsDb") != -1 and self.blogPosts.db.current_config == None:
+            return redirect(url_for("db_setup.set_database"))
     
-    def create_bp(self):
-        home = Blueprint("home", __name__)
+    def front_page(self):
+        rows = len(self.blogPosts)
+        if rows == 0:
+            return render_template("home.html", allposts = placeholder.get_all())
 
-        @home.before_request
-        def goto_db_setup():
-            if str(type(self.blogPosts)).find("PostsDb") != -1 and self.blogPosts.db.current_config == None:
-                return redirect(url_for("db_setup.set_database"))
-        
-        @home.route("/")
-        def front_page():
-            rows = len(self.blogPosts)
-            if rows == 0:
-                return render_template("home.html", allposts = placeholder.get_all())
-
-            return render_template("home.html", allposts = self.blogPosts.get_all())
-
-        return home
-    
+        return render_template("home.html", allposts = self.blogPosts.get_all())
--- view/post_view.py
+++ view/post_view.py
@@ -1,58 +1,57 @@
 from flask import Blueprint, render_template, request, redirect, url_for, flash
 from models.post import Post
 
-
 class PostPage:
     def __init__(self, allPosts):
         self.blogPosts = allPosts
-        self.bp = self.create_bp()           
-                
-    def create_bp(self):
-        post = Blueprint("posts", __name__)
-        @post.before_request
-        def goto_db_setup():
-            if str(type(self.blogPosts)).find("PostsDb") != -1 and self.blogPosts.db.current_config == None:
-                return redirect(url_for("db_setup.set_database"))
+        self.bp = Blueprint("posts", __name__)
+        self.to_db_setup = self.bp.before_request(self.goto_db_setup)
+        self.creation = self.register("/create",self.create)
+        self.reading = self.register("/read/<post_id>", self.read)
+        self.update = self.register("/edit/<post_id>", self.edit)     
 
-        @post.route("/create", methods = ["Get", "Post"])
-        def create():
-            if request.method == "POST":
-                self.create_new_post()
-                return redirect(f"/post/read/{self.blogPosts.get_ids()[-1]}")
+    def register(self, link, func):
+        return self.bp.route(link, methods = ["Get", "Post"])(func)
 
-            return render_template("writePost.html")
+    def goto_db_setup(self):
+        if str(type(self.blogPosts)).find("PostsDb") != -1 and self.blogPosts.db.current_config == None:
+            return redirect(url_for("db_setup.set_database"))
 
-        @post.route("/read/<post_id>",methods = ["Get", "Post"])
-        def read(post_id):
-            if request.method == "POST":
-                to_delete = request.form.get("postID")
-                self.blogPosts.remove(to_delete)
-                flash("Your post has been successfully removed.", "info")
-                return redirect(url_for("home.front_page"))
-            
-            selected_post = self.blogPosts.get_post(post_id)
-            return render_template(
-                "read.html",
-                editable = post_id,
-                title = selected_post.title,
-                auth = selected_post.auth,
-                content = selected_post.content,
-                date = selected_post.date)
+    def create(self):
+        if request.method == "POST":
+            self.create_new_post()
+            return redirect(f"/post/read/{self.blogPosts.get_ids()[-1]}")
 
-        @post.route("/edit/<post_id>", methods = ["Get", "Post"])
-        def edit(post_id):
-            selected_post = self.blogPosts.get_post(post_id)
-            if request.method == "POST":
-                self.edit_post(post_id)
-                return redirect(f"/post/read/{post_id}")
-            return render_template(
-                "edit.html",
-                auth = selected_post.auth,
-                title = selected_post.title,
-                current = selected_post.content,
-                date = selected_post.date)
-        return post
-    
+        return render_template("writePost.html")
+
+    def read(self, post_id):
+        if request.method == "POST":
+            to_delete = request.form.get("postID")
+            self.blogPosts.remove(to_delete)
+            flash("Your post has been successfully removed.", "info")
+            return redirect(url_for("home.front_page"))
+        
+        selected_post = self.blogPosts.get_post(post_id)
+        return render_template(
+            "read.html",
+            editable = post_id,
+            title = selected_post.title,
+            auth = selected_post.auth,
+            content = selected_post.content,
+            date = selected_post.date)
+
+    def edit(self, post_id):
+        selected_post = self.blogPosts.get_post(post_id)
+        if request.method == "POST":
+            self.edit_post(post_id)
+            return redirect(f"/post/read/{post_id}")
+        return render_template(
+            "edit.html",
+            auth = selected_post.auth,
+            title = selected_post.title,
+            current = selected_post.content,
+            date = selected_post.date)  
+
     def create_new_post(self):
         author = request.form.get("author")
         title = request.form.get("title")
@@ -64,5 +63,4 @@ class PostPage:
         title = request.form.get("title")
         content = request.form.get("post")
         editted = Post(author, title, content)
-        self.blogPosts.replace(post_id, editted)
-        
+        self.blogPosts.replace(post_id, editted)    
