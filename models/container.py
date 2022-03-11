4da4ff5587a52c4d1b0b03e4512e3ce0e034bf7d
sergiu.botezatu95@gmail.com <sergiu.botezatu95@gmail.com>
2022-03-11 04:50:31 +0200

    small edits + failed mocking

--- models/container.py
+++ models/container.py
@@ -1,3 +1,4 @@
+from importlib.util import spec_from_file_location
 from unittest import mock
 from resources.posts_db_repo import PostsDb
 from resources.posts_in_memo import Posts
@@ -6,6 +7,13 @@ from resources.idata_base import IDataBase
 from resources.ipost_repo import IPostRepo
 from resources.idatabase_config import IDataBaseConfig
 from resources.database_config import DataBaseConfig
+from resources.services import Services
+
+@mock.patch("resources.database_config.DataBaseConfig", "is_configured", True)
+@Services.get
+def mocked_db_config(mocked):
+    mocked = mock.MagicMock(spec= ["add_settings"])
+    return mocked.return_value
 
 class Container:
     DEPENDENCIES = {
@@ -21,17 +29,8 @@ class Container:
 
         self.test_services = {
             IPostRepo : Posts,
-            IDataBase: self.mocked_dataBase,
-            IDataBaseConfig: self.mocked_db_config}
+            IDataBase: DataBase,
+            IDataBaseConfig: mocked_db_config}
         
     def get(self, is_test : bool) -> dict:
         return self.test_services if is_test else self.prod_services
-
-    def mocked_dataBase(self):
-        mock_db = mock.create_autospec(DataBase)
-        return mock_db
-
-    def mocked_db_config(self):
-        mock_config = mock.create_autospec(DataBaseConfig)
-        return mock_config
-
