8b1c8c40f23aa1813d864d8a223e03d6bc8b5233
sergiu.botezatu95@gmail.com <sergiu.botezatu95@gmail.com>
2022-03-15 16:27:25 +0200

    posts_tests fixed

--- services/mock_db_config.py
+++ services/mock_db_config.py
@@ -5,7 +5,7 @@ class MockDbConfig:
     @staticmethod
     @Services.get
     def mocked_db_config(self):
-        with mock.patch("resources.database_config.DataBaseConfig") as mocked_config:
+        with mock.patch("services.database_config.DataBaseConfig") as mocked_config:
             mocked_config.is_configured = False
             mocked_config.add_settings = MockDbConfig.mock_add_settings
             mocked_config.load = MockDbConfig.mock_load 
