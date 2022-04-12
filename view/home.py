ef0304bece4535f335a3f85819f241dc51fe7c3b
sergiu.botezatu95@gmail.com <sergiu.botezatu95@gmail.com>
2022-04-12 13:54:30 +0300

    settings.json - not removed from git yet

--- view/home.py
+++ view/home.py
@@ -5,44 +5,50 @@ from services.posts.seed import placeholder
 from services.dependency_inject.injector import Services
 
 class Home:
-    i = 0
     @Services.get
     def __init__(self, repo : IPostRepo):
         self.blogPosts = repo
         self.bp = Blueprint("home", __name__)
         self.home = self.bp.route("/", methods = ["GET", "POST"])(self.front_page)
         self.to_db_setup = self.bp.before_request(self.goto_db_setup)
-        self.posting_users = []
-        self.filters = []
+        self.filtered = set()
+        self.not_filtered = set()
         
     def goto_db_setup(self):
         if not DataBase.config.is_configured:
             return redirect(url_for("db_setup.set_database"))
     
     def front_page(self):
-        id = None
-        if request.method == "POST":
-            id = request.form.get("user")
-            name = request.form.get("name")
-            if id != "x":
-                self.filters.append((id, name))
-                self.posting_users.remove((int(id), name))
-                query_url = "?"
-                i = 0
-                for filter in self.filters:
-                    query_url += f"usr_id{i}={filter[0]}&"
-                return redirect(f"/{query_url}")
-            else:
-                usr_id = request.form.get("id")
-                self.filters.remove((usr_id, name))
-                self.posting_users.append((int(usr_id), name))
-            return render_template("home.html", filters = self.filters, users = self.posting_users, allposts = self.blogPosts.get_all())
         rows = len(self.blogPosts)
         posts = None
+        filters = request.args.getlist("user_id")
+        self.filtered = set()
+        if request.method == "POST":
+            id = request.form.get("id")
+            if id == "x":
+                unfilter_id = request.form.get("rmv_id")
+                filters.remove(unfilter_id)
+            else:
+                filters.append(id)
+                print(id)
+            query_url = ""
+            separator = ""
+            for filter in filters:
+                query_url += f"{separator}{filter}"
+                separator = "&user_id="
+            route = f"/?user_id={query_url}" if query_url != "" else "/"
+            return redirect(route)
         if rows > 0:
-            posts = self.blogPosts
-            self.posting_users = self.blogPosts.get_with_posts()
-            getter = posts.get_user_posts(id) if id != None else posts.get_all()
+            if filters != []:
+                posts = self.blogPosts.get_all(filters)
+                for post in posts:
+                    self.filtered.add((post[1].owner_id, post[1].auth))
+                self.not_filtered.difference_update(self.filtered)
+            else:
+                posts = self.blogPosts.get_all()
+                for post in posts:
+                    self.not_filtered.add((post[1].owner_id, post[1].auth))
         else:
-            posts = placeholder
-        return render_template("home.html", filters = self.filters, users = self.posting_users, allposts = getter)
+            posts = placeholder.get_all()
+        print(self.not_filtered)
+        return render_template("home.html", allposts = posts, filters = self.filtered, users = self.not_filtered)
