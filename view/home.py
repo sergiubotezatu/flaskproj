c620a354171590868fb3d39731b564bd6ce566f6
sergiu.botezatu95@gmail.com <sergiu.botezatu95@gmail.com>
2022-04-15 13:02:01 +0300

    filters refactored

--- view/home.py
+++ view/home.py
@@ -27,36 +27,12 @@ class Home:
         if rows > 0 :
             filtered_ids = filters["user_id"]
             filtered_names = filters["name"]
-            print(self.not_filtered)
-            print("1xx", filters)
-            print("2xx", filtered_ids)
-            print("3xx", filtered_names)
+            self.__update_not_filtered(filtered_ids, filtered_names)
             posts = self.blogPosts.get_all(filtered_ids)
-            if filtered_ids == []:
-                self.not_filtered = self.get_not_filtered_users()
-            for i in range(0, len(filtered_ids)):
-                self.not_filtered.discard((filtered_ids[i], filtered_names[i]))
         else:
             posts = placeholder.get_all()
         if request.method == "POST":
-            query_url = "?"
-            id = request.form.get("user_id")
-            print("DD", id)
-            name = request.form.get("name")
-            if id == "x":
-                id = request.form.get("rmv_id")
-                self.not_filtered.add((id, name))
-                filtered_ids.remove(id)
-                filtered_names.remove(name)
-                filters.update({"user_id" : filtered_ids})
-                filters.update({"name" : filtered_names})
-                query_url += urlencode(filters, doseq=True)
-            else:
-                print("lung", len(filters))
-                new_filter = f"user_id={id}&name={name}" if len(filters["user_id"]) == 0 else f"&user_id={id}&name={name}"
-                query_url += urlencode(filters, doseq=True) + new_filter
-            return redirect(f"/{query_url}")
-        print("AA", self.not_filtered)
+            return self.__add_remove_filters(filtered_ids, filtered_names, filters)
         return render_template("home.html", allposts = posts, filters = filters, users = self.not_filtered)
 
     def get_not_filtered_users(self) -> set:
@@ -65,3 +41,26 @@ class Home:
             for post in self.blogPosts.get_all():
                 result.add((str(post[1].owner_id), post[1].auth))
         return result
+
+    def __add_remove_filters(self, ids : list, names : list, filters : defaultdict):
+        query_url = "?"
+        id = request.form.get("user_id")
+        name = request.form.get("name")
+        if id == "x":
+            id = request.form.get("rmv_id")
+            self.not_filtered.add((id, name))
+            ids.remove(id)
+            names.remove(name)
+            filters.update({"user_id" : ids})
+            filters.update({"name" : names})
+            query_url += urlencode(filters, doseq=True)
+        else:
+            new_filter = f"user_id={id}&name={name}" if len(filters["user_id"]) == 0 else f"&user_id={id}&name={name}"
+            query_url += urlencode(filters, doseq=True) + new_filter
+        return redirect(f"/{query_url}")
+
+    def __update_not_filtered(self, ids, names):
+        if ids == []:
+            self.not_filtered = self.get_not_filtered_users()
+        for i in range(0, len(ids)):
+            self.not_filtered.discard((ids[i], names[i]))
