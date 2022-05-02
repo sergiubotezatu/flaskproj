2db9d86db39316f4c28887df39527a7e02869f56
sergiubotezatu <sergiu.botezatu95@gmail.com>
2022-05-02 16:20:33 +0300

    added pagination tests

--- tests/test_pagination.py
+++ tests/test_pagination.py
@@ -32,23 +32,49 @@ class PaginationTests(unittest.TestCase):
             self.test_app = current_app.test_client()
             self.ctx.push()
 
-    def create_posts(self, name, count :int):
+    def create_posts(self, name, count :int, title = "Generic-1"):
             post = {
             "author" : name,
-            "title" : "Generic",
+            "title" : title,
             "post" : "This is a test"
             }
             for i in range(0, count):
-                post["title"] = post["title"] + str(i)
+                post["title"] = post["title"].replace(str(i - 1), str(i))
                 self.test_app.post("/post/create", data = post, follow_redirects=False)
+        
+    @log_user(2, "John", "John@mail", "regular")
+    @configure(True)
+    def test_next_button_available_if_more_posts(self):
+        self.create_posts("John", 6)
+        home_pg = self.test_app.get("/")
+        self.assertIn("Next &raquo;", home_pg.data.decode("UTF-8"))
+
+    @log_user(2, "John", "John@mail", "regular")
+    @configure(True)
+    def test_next_button_notavailable_if_less_than_6posts(self):
+        self.test_app.post("/post/read/1", data = {"postID" : "1"})
+        self.test_app.post("/post/read/2", data = {"postID" : "2"})
+        home_pg = self.test_app.get("/")
+        self.assertNotIn("Next &raquo;", home_pg.data.decode("UTF-8"))
     
     @log_user(2, "John", "John@mail", "regular")
     @configure(True)
     def test_only_5posts_displayed_on_pg(self):
-        self.create_posts("John", 6)
-        self.test_app.get(url_for("authentication.log_out"))
+        self.create_posts("John", 1, title = "Generic1")
         home_pg = self.test_app.get("/")
-        something = home_pg.data.decode("UTF-8")
-        self.assertEqual(5, home_pg.data.decode("UTF-8").count("John"))
+        self.assertIn("Generic5", home_pg.data.decode("UTF-8"))
         self.assertIn("Generic4", home_pg.data.decode("UTF-8"))
-        self.assertNotIn("Generic5", home_pg.data.decode("UTF-8"))
+        self.assertIn("Generic3", home_pg.data.decode("UTF-8"))
+        self.assertIn("Generic2", home_pg.data.decode("UTF-8"))
+        self.assertIn("Generic1", home_pg.data.decode("UTF-8"))
+        self.assertNotIn("Generic0", home_pg.data.decode("UTF-8"))
+
+    @configure(True)
+    def test_previous_button_available_on_second_pg(self):
+        home_pg = self.test_app.get("/?pg=2")
+        self.assertIn("&laquo; Previous", home_pg.data.decode("UTF-8"))
+
+    @configure(True)
+    def test_previous_button_notavailable_on_first_pg(self):
+        home_pg = self.test_app.get("/")
+        self.assertNotIn("&laquo; Previous", home_pg.data.decode("UTF-8"))
