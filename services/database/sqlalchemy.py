d36f8bb5c7167e589bf34c5dfa4766d29ce24710
sergiubotezatu <sergiu.botezatu95@gmail.com>
2022-05-13 03:19:05 +0300

    sqlalchemy extends database/not using flasksqlalch

--- services/database/sqlalchemy.py
+++ services/database/sqlalchemy.py
@@ -1,23 +1,36 @@
-from flask_sqlalchemy import SQLAlchemy
-from models.db_settings import DBSettings
-from flask import current_app
-from typing import Text
 from sqlalchemy.ext.automap import automap_base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from services.database.database import DataBase
+from services.dependency_inject.injector import Services
+from services.interfaces.idata_base import IDataBase
+from services.interfaces.idatabase_config import IDataBaseConfig
+from services.interfaces.idb_upgrade import IDataBaseUpgrade
 
-class SqlAlchemy:
-    db  : SQLAlchemy = None
+class SqlAlchemy(DataBase, IDataBase):
+    session = None
     Users = None
     Posts = None
     Deleted = None
+    @Services.get
+    def __init__(self, config : IDataBaseConfig, upgrader : IDataBaseUpgrade):
+        super(SqlAlchemy, self).__init__(config, upgrader)
+                
+    def set_db(self):
+        super().set_db()
+        self.register_sqlalchemy()
+        self.load_models()
+
+    def register_sqlalchemy(self):
+        SQLALCHEMY_DATABASE_URI = self.db_settings.to_DB_URI()
+        SqlAlchemy.engine = create_engine(SQLALCHEMY_DATABASE_URI)
+        Session = sessionmaker(autocommit=False, autoflush=False, bind=SqlAlchemy.engine)
+        SqlAlchemy.session = Session()        
 
     @classmethod
-    def configure(cls, settings : DBSettings):
-        current_app.config["SQLALCHEMY_DATABASE_URI"] = settings.to_DB_URI()
-        current_app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-        cls.db = SQLAlchemy(current_app)
-        db = cls.db
+    def load_models(cls):
         Base = automap_base()
-        Base.prepare(db.engine, reflect = True)
+        Base.prepare(cls.engine, reflect = True)
         cls.Users = Base.classes.blog_users
         cls.Posts = Base.classes.blog_posts
         cls.Deleted = Base.classes.deleted_users
