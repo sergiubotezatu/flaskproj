17ed504a7a2fc494c23f88ff239cf3db0656015a
sergiubotezatu <sergiu.botezatu95@gmail.com>
2022-06-07 13:31:55 +0300

    removed just_add/delete img /default not editable

--- services/images.py
+++ services/images.py
@@ -1,22 +1,28 @@
 import uuid
 from werkzeug.datastructures import FileStorage
+import os
 
 class Images:
     def __init__(self):
         self.PATH = "static/images"
-        self.DEFAULT = "/static/images/noimage.png"
-        self.just_added = None
+        self.DEFAULT = "/static/images/noimage.jpg"
        
     def add(self, pic : FileStorage):
-        self.just_added = str(uuid.uuid4())
-        extension = "." + pic.mimetype.partition("/")[2]
-        self.just_added += extension
-        self.__add_on_disk(pic, self.just_added)
+        just_added = self.__create_name(pic)
+        self.__add_on_disk(pic, just_added)
+        return just_added
 
     def edit(self, pic : FileStorage, path : str):
         image = pic.read()
-        with open(path[1:], "wb") as writter:
+        is_default = "noimage" in path
+        name = self.__create_name(pic)
+        new_path = path[1:] if not is_default else self.PATH + f"/{name}"
+        with open(new_path, "wb") as writter:
             writter.write(image)
+        if is_default:
+            return name
+        else:
+            return None
 
     def get(self, file_name):
         if file_name == None:
@@ -24,7 +30,15 @@ class Images:
         pic_path = f"/{self.PATH}/{file_name}"
         return pic_path
 
+    def remove(self, file_name):
+        if file_name:
+            os.remove(f"{self.PATH}/{file_name}")
+
     def __add_on_disk(self, pic : FileStorage, file_name : str):
         image = pic.read()
         with open(self.PATH + f"/{file_name}", "wb") as writter:
             writter.write(image)
+
+    def __create_name(self, pic):
+        extension = "." + pic.mimetype.partition("/")[2]
+        return str(uuid.uuid4()) + extension
