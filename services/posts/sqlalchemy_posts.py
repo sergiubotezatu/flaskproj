e9c3af07827aa8cf203757ed3f1875ebfeaddd93
sergiubotezatu <sergiu.botezatu95@gmail.com>
2022-06-07 13:33:08 +0300

    updated sql_alchemy_posts repo

--- services/posts/sqlalchemy_posts.py
+++ services/posts/sqlalchemy_posts.py
@@ -1,6 +1,4 @@
-import base64
 from sqlalchemy import func
-from models.image import Image
 from services.database.sqlalchemy import SqlAlchemy
 from services.images import Images
 from services.interfaces.idata_base import IDataBase
@@ -12,15 +10,14 @@ from werkzeug.datastructures import FileStorage
 class SqlAlchemyPosts(IPostRepo):
     @Services.get
     def __init__(self, orm : IDataBase):
-        self.session = orm.session
-        self.posts = orm.Posts
+        self.orm = orm
         self.images = Images()
         self.__count = -1
         
     @property
     def count(self):
         if self.__count == -1:
-            return self.session.query(self.posts.postid).count()
+            return self.orm.session.query(self.orm.Posts.postid).count()
         return self.__count
 
     @count.setter
@@ -32,54 +29,56 @@ class SqlAlchemyPosts(IPostRepo):
     
     def add(self, post : Post, img : FileStorage):
         self.count += 1
-        self.add_image(img)
-        new_post = self.posts(title = post.title,
+        new_post = self.orm.Posts(title = post.title,
                             content = post.content,
                             date = post.created,
                             ownerid = post.owner_id,
-                            image = self.images.just_added)
-        self.session.add(new_post)
-        self.session.commit()
+                            image = self.get_image(img))
+        self.orm.session.add(new_post)
+        self.orm.session.commit()
         return new_post.postid
 
-    def add_image(self, img : FileStorage):
-        if img != None:
-            self.images.add(img)
+    def get_image(self, img : FileStorage):
+        if img:
+            return self.images.add(img)
 
-    def replace(self, id, post : Post = None, img : FileStorage = None, img_path : str = ""):
-        dict_new = {}
-        if img != None:
-            self.images.edit(img, img_path)
-        else:
-            dict_new = {"title" : post.title,
-                        "content" : post.content,
-                        "date_modified" : post.created}
-            self.session.query(self.posts).filter_by(postid = id).update(dict_new)
-        self.session.commit()
+    def replace(self, post : Post, img : FileStorage):
+        dict_new = {"title" : post.title,
+                    "content" : post.content,
+                    "date_modified" : post.created}
+        if img:
+            changed_name = self.images.edit(img, post.img_path)
+            if changed_name:
+                dict_new.update({"image" : changed_name})
+        self.orm.session.query(self.orm.Posts).filter_by(postid = post.id).update(dict_new)
+        self.orm.session.commit()
 
     def remove(self, id):
-        self.session.query(self.posts).filter_by(postid = id).delete()
-        self.session.commit()
-    
+        img = self.orm.session.query(self.orm.Posts.image).filter_by(postid = id).first()
+        self.orm.session.query(self.orm.Posts).filter_by(postid = id).delete()
+        self.orm.session.commit()
+        self.images.remove(img[0])
+        
     def get(self, id, email = None) -> Post:
         displayed = None
         post = None
         if email != None:
-            displayed = self.session.query(SqlAlchemy.Deleted.content).filter_by(email = email, deletedid = id).first()
+            displayed = self.orm.session.query(SqlAlchemy.Deleted.content).filter_by(email = email, deletedid = id).first()
             post = Post(email, "No title", displayed[0])
         else:
-            displayed = self.session.query\
+            displayed = self.orm.session.query\
                         (SqlAlchemy.Users.name,
-                        self.posts.title,
-                        self.posts.content,
+                        self.orm.Posts.title,
+                        self.orm.Posts.content,
                         SqlAlchemy.Users.ownerid,
-                        self.posts.date,
-                        self.posts.date_modified,
-                        self.posts.image).\
-                        join(SqlAlchemy.Users, SqlAlchemy.Users.ownerid == self.posts.ownerid).\
-                        filter(self.posts.postid == id).first()
+                        self.orm.Posts.date,
+                        self.orm.Posts.date_modified,
+                        self.orm.Posts.image).\
+                        join(SqlAlchemy.Users, SqlAlchemy.Users.ownerid == self.orm.Posts.ownerid).\
+                        filter(self.orm.Posts.postid == id).first()
             img = self.get_img(displayed[6])
             post = Post(displayed[0], displayed[1], displayed[2], owner_id = displayed[3], date = displayed[4], img_path=img)
+            post.id = id
             post.modified = displayed[5]
         return post
 
@@ -87,25 +86,25 @@ class SqlAlchemyPosts(IPostRepo):
         return self.images.get(img_name)
         
     def get_all(self, page = 0, filters : list = [], max = 5):
-        posts = self.session.query\
-            (self.posts.postid,
+        posts = self.orm.session.query\
+            (self.orm.Posts.postid,
             SqlAlchemy.Users.name,
-            self.posts.title,
-            func.substr(self.posts.content, 0, 150),
-            self.posts.ownerid,
-            self.posts.date,
-            self.posts.image).\
-            join(SqlAlchemy.Users, SqlAlchemy.Users.ownerid == self.posts.ownerid).\
-            order_by(self.posts.postid.desc())
+            self.orm.Posts.title,
+            func.substr(self.orm.Posts.content, 0, 150),
+            self.orm.Posts.ownerid,
+            self.orm.Posts.date,
+            self.orm.Posts.image).\
+            join(SqlAlchemy.Users, SqlAlchemy.Users.ownerid == self.orm.Posts.ownerid).\
+            order_by(self.orm.Posts.postid.desc())
         if len(filters) > 0:
-            posts = posts.filter(self.posts.ownerid.in_(filters))
+            posts = posts.filter(self.orm.Posts.ownerid.in_(filters))
         if page > 0:
             offset = page * max - max
             posts = posts.offset(offset).limit(max + 1)
         return self.__get_fetched(posts.all())
 
     def unarchive_content(self, id, name, email):
-        result = self.session.query(SqlAlchemy.Deleted.content).filter_by(email = email).all()
+        result = self.orm.session.query(SqlAlchemy.Deleted.content).filter_by(email = email).all()
         i = 1
         for post in result:
             if post[0] != None:
