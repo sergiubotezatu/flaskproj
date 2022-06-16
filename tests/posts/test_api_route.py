963e0b900dbb1641c18ea39311a3a2a348fb3937
sergiubotezatu <sergiu.botezatu95@gmail.com>
2022-06-16 15:43:46 +0300

    added api_route_tests

--- tests/posts/test_api_route.py
+++ tests/posts/test_api_route.py
@@ -16,9 +16,25 @@ def test_succesful_request(client):
     assert result.status_code == 200
 
 @configure(True)
-def test_post_not_rendered_on_server(client):
-    pass
+def test_api_route_returns_json(client):
+    result = client.get("/api/post/1/")
+    assert result.headers[0] == ('Content-Type', 'application/json')
+    
+@configure(True)
+def test_api_route_returns_correct_post(client):
+    result = client.get("/api/post/1/")
+    json = result.json
+    assert "Mark Doe" == json["auth"]
+    assert "Test post 1" == json["content"]
+    assert "Generic1" == json["title"]
 
 @configure(True)
-def test_route_returns_json(client):
-    pass
+def test_404_if_post_not_found(client):
+    result = client.get("/api/post/12/")
+    json = result.json
+    assert "404. NOT FOUND" == json["error"]
+
+@configure(True)
+def test_html_not_rendered_on_server_side(client):
+    result = client.get("/post/api/1/")
+    assert "Mark Doe" not in result.data.decode("UTF-8")
