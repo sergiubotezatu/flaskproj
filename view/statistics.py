ddfbeeb0d578483e89451ace299c4cdaeab05630
sergiubotezatu <sergiu.botezatu95@gmail.com>
2022-06-17 13:20:53 +0300

    statistics access decorator + corrected error

--- view/statistics.py
+++ view/statistics.py
@@ -4,11 +4,16 @@ from datetime import datetime
 from flask import Blueprint, render_template
 from models.user import User
 from services.dependency_inject.injector import Services
+from services.interfaces.iauthorization import IAuthorization
 from services.interfaces.ifilters import IFilters
+from services.interfaces.isession_mngr import ISessionMNGR
 from services.interfaces.iusers_repo import IUsersRepo
+from services.users.access_decorators import AccessDecorators
 
 
 class UserStatistics:
+    access = AccessDecorators(IAuthorization, ISessionMNGR)
+
     @Services.get
     def __init__(self, filter : IFilters, users : IUsersRepo):
         self.filter = filter
@@ -18,6 +23,7 @@ class UserStatistics:
         self.organizer = defaultdict(lambda: 0)
         self.MONTHS = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
 
+    @access.member_required
     def get_statistic(self, user_id):
         user = self.users.get_by(id = user_id)
         self.create_organizer(user)
@@ -29,7 +35,7 @@ class UserStatistics:
     def create_organizer(self, user : User):
         start_date = datetime.strptime(user.created, "%d/%b/%y %H:%M:%S").date()
         self.__generate_month_tracker(start_date)
-        posts = self.filter.apply({"user_id" : [user.id], "name" : [user.name]}, -1)
+        posts = self.filter.apply({"user_id" : [str(user.id)], "name" : [user.name]}, -1)
         for post in posts:
             date = post[1].created[3:9]
             self.organizer[date] += 1
@@ -38,10 +44,10 @@ class UserStatistics:
         end_date = datetime.now()
         year_lapse = end_date.year - start_date.year
         month_lapse = year_lapse * 12 + end_date.month - start_date.month
-        i = 0 if month_lapse > 0 else - 1
+        i = 0
         month_index = start_date.month - 1
         year = int(start_date.year)
-        while i < month_lapse:
+        while i <= month_lapse:
             if month_index == 12:
                 month_index = 0
                 year += 1
