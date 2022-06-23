9c692dd2108e57617b6cb1e6f116cfa0827ad992
sergiu <sergiu.botezatu95@gmail.com>
2022-06-24 02:12:13 +0300

    modified assertion

--- tests/users/test_user_statistics.py
+++ tests/users/test_user_statistics.py
@@ -4,17 +4,20 @@ from services.users.users_in_memo import Users
 
 from tests.helpers import configure, log_user
 
+
 @fixture()
-def client(data_base : Flask):
+def client(data_base: Flask):
     return data_base.test_client()
+
 
 @log_user(1, "Mark", "Mark@mail", "regular")
 @configure(True)
 def test_onefield_only_if_user_created_this_month(client):
     read_stats = client.get("/user/statistics/1/")
-    assert (
-    "<th>No.ofposts</th>\n</tr>\n\n<tr>\n<td>Jun/22</td>\n<td>3</td>\n</tr>\n\n</table>\n"
-    in read_stats.data.decode("UTF-8").replace(" ", ""))
+    result = read_stats.data.decode("UTF-8").replace(" ", "")
+    expected = "<th>No.ofposts</th>\n</tr>\n\n\n<tr>\n<td>Jun/22</td>\n<td>3</td>\n</tr>\n\n</table>"
+    assert expected in result
+
 
 @log_user(1, "Mark", "Mark@mail", "regular")
 @configure(True)
@@ -22,9 +25,10 @@ def test_statistics_displayed_correctly_for_one_month_user(client):
     read_stats = client.get("/user/statistics/1/")
     assert "<td>Jun/22</td>\n<td>3</td>\n" in read_stats.data.decode("UTF-8").replace(" ", "")
 
+
 @log_user(1, "Mark", "Mark@mail", "regular")
 @configure(True)
 def test_statistics_displays_only_months_with_activity(client):
-    Users().get_by(id = "1").created = "17/Mar/22 11:32:25"
+    Users().get_by(id="1").created = "17/Mar/22 11:32:25"
     read_stats = client.get(f"/user/statistics/1/")
     assert "<td>Jun/22</td>" in read_stats.data.decode("UTF-8")
